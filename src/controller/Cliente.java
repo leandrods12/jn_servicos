b237c8b07b798a727e81229509a8b9cfaeedda78
leandrods12 <leandrods12@gmail.com>
2020-08-07 22:29:22 -0300

    atulizando classe cliente

--- src/controller/Cliente.java
+++ src/controller/Cliente.java
@@ -1,5 +1,40 @@
 package controller;
 
 public class Cliente {
-
+	private String nome;
+	private long cpf;
+	private String endereco;
+	private long telefone;
+	public Cliente(String nome, String endereco, long cpf, long telefone) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.telefone = telefone;
+				
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getCpf() {
+		return cpf;
+	}
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	public double getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(long telefone) {
+		this.telefone = telefone;
+	}
+	
 }
