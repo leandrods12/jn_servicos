722a302d232c5f7733ce7f347d5bb942db7d6542
Gustavo Simões <gustavoteixeira@gec.inatel.br>
2020-08-07 22:12:54 -0300

    Adcionada a Classe de Acesso ao BD

---
+++ src/model/GeralDAO.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gustavo
+ */
+public class GeralDAO {
+    
+    /**
+     * *************** CONEXÃO COM O BANCO DE DADOS ***********************
+     */
+    // objeto responsável pela conexão com o servidor do banco de dados
+    Connection con;
+    // objeto responsável por preparar as consultas dinâmicas
+    PreparedStatement pst;
+    // objeto responsável por executar as consultas estáticas
+    Statement st;
+    // objeto responsável por referenciar a tabela resultante da busca
+    ResultSet rs;
+
+    // NOME DO BANCO DE DADOS
+    String database = "GerenciadorDeEstoque";
+    // URL: VERIFICAR QUAL A PORTA
+    String url = "jdbc:Mysql://localhost:3306/" + database + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
+    // USUÁRIO
+    String user = "root";
+    // SENHA
+    String password = "gustavo16";
+
+    boolean sucesso = false;
+
+    // Conectar ao banco de dados
+    public void connectToDb() {
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro: " + ex.getMessage(), "Mensagem de Erro", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+}
