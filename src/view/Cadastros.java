79ce1656adc9f883e1f8770656d06c05a7613e55
Gustavo Simões <gustavoteixeira@gec.inatel.br>
2020-10-25 12:27:38 -0300

    Atualização de Telas

--- src/view/Cadastros.java
+++ src/view/Cadastros.java
@@ -81,7 +81,8 @@ public class Cadastros extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        Servicos servicos = new Servicos();
+        servicos.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
